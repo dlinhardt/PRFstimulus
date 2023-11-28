413e2de5aa28c4c99584f5cb25adf872c711b04a
Sonboth <linhardtdavid@gmail.com>
2023-11-28 11:26:30 +0100

    added fixed continout multiplyer, autoformat

--- barStimulus.py
+++ barStimulus.py
@@ -22,8 +22,8 @@ class barStimulus(Stimulus):
         loadImages=None,
         flickerFrequency=8,
         forceBarWidth=None,
+        continous_multiplier=2,
     ):
-
         super().__init__(
             stimSize,
             maxEcc,
@@ -34,7 +34,7 @@ class barStimulus(Stimulus):
             flickerFrequency=flickerFrequency,
             continous=continous,
         )
-        self.stimulus_type = 'bar'
+        self.stimulus_type = "bar"
 
         self.nBars = nBars
         self.doubleBarRot = doubleBarRot
@@ -51,17 +51,25 @@ class barStimulus(Stimulus):
         )
 
         if self.forceBarWidth is not None:
-            forceBarWidthPix = np.ceil(self.forceBarWidth / (2 * self._maxEcc) * self._stimSize).astype('int')
-            self.barWidth = forceBarWidthPix
-            self.overlap  = (self._stimSize + 0.5) / (self.barWidth * self.framesPerCrossing)
+            forceBarWidthPix = np.ceil(
+                self.forceBarWidth / (2 * self._maxEcc) * self._stimSize
+            ).astype("int")
+            self.bar_width = forceBarWidthPix
+            self.overlap = (self._stimSize + 0.5) / (
+                self.bar_width * self.framesPerCrossing
+            )
         else:
-            self.overlap  = overlap
-            self.barWidth = np.ceil(self._stimSize / (self.framesPerCrossing * self.overlap - 0.5)).astype('int')
+            self.overlap = overlap
+            self.bar_width = np.ceil(
+                self._stimSize / (self.framesPerCrossing * self.overlap - 0.5)
+            ).astype("int")
 
         if not continous:
             self.continous = False
             self._stimRaw = np.zeros((self.nFrames, self._stimSize, self._stimSize))
             self._stimBase = np.zeros(self.nFrames)  # to find which checkerboard to use
+
+            self.jump_size = self.overlap * self.bar_width
 
             it = 0
             for cross in self.startingDirection:
@@ -69,12 +77,9 @@ class barStimulus(Stimulus):
                     frame = np.zeros((self._stimSize, self._stimSize))
                     frame[
                         :,
-                        max(0, int(self.overlap * self.barWidth * (i - 1))) : min(
+                        max(0, int(self.jump_size * (i - 1))) : min(
                             self._stimSize,
-                            int(
-                                self.overlap * self.barWidth * (i - 1) +
-                                self.barWidth
-                            ),
+                            int(self.jump_size * (i - 1) + self.bar_width),
                         ),
                     ] = 1
 
@@ -88,35 +93,35 @@ class barStimulus(Stimulus):
                                 o = max(
                                     int(np.ceil(self._stimSize / 2 - 1)),
                                     int(
-                                        self.overlap * self.barWidth * (i - 1) +
-                                        self.barWidth * self.thickRatio * 0.55 +
-                                        self.nBarShift * (nbar + 1)
+                                        self.jump_size * (i - 1)
+                                        + self.bar_width * self.thickRatio * 0.55
+                                        + self.nBarShift * (nbar + 1)
                                     ),
                                 )
                                 t = int(
-                                    self.overlap * self.barWidth * (i - 1) +
-                                    self.barWidth * self.thickRatio +
-                                    self.nBarShift * (nbar + 1)
+                                    self.jump_size * (i - 1)
+                                    + self.bar_width * self.thickRatio
+                                    + self.nBarShift * (nbar + 1)
                                 )
                             elif i == self.framesPerCrossing - 1:
                                 o = int(
-                                    self.overlap * self.barWidth * (i - 1) +
-                                    self.nBarShift * (nbar + 1)
+                                    self.jump_size * (i - 1)
+                                    + self.nBarShift * (nbar + 1)
                                 )
                                 t = int(
-                                    self.overlap * self.barWidth * (i - 1) +
-                                    self.barWidth * self.thickRatio * 0.45 +
-                                    self.nBarShift * (nbar + 1)
+                                    self.jump_size * (i - 1)
+                                    + self.bar_width * self.thickRatio * 0.45
+                                    + self.nBarShift * (nbar + 1)
                                 )
                             else:
                                 o = int(
-                                    self.overlap * self.barWidth * (i - 1) +
-                                    self.nBarShift * (nbar + 1)
+                                    self.jump_size * (i - 1)
+                                    + self.nBarShift * (nbar + 1)
                                 )
                                 t = int(
-                                    self.overlap * self.barWidth * (i - 1) +
-                                    self.barWidth * self.thickRatio +
-                                    self.nBarShift * (nbar + 1)
+                                    self.jump_size * (i - 1)
+                                    + self.bar_width * self.thickRatio
+                                    + self.nBarShift * (nbar + 1)
                                 )
 
                             frame2[:, max(0, o) : min(self._stimSize, t)] = 1
@@ -153,7 +158,7 @@ class barStimulus(Stimulus):
 
         else:
             self.continous = True
-            self.frameMultiplier = self.TR * 16 # hard set the frameMultiplier to TR*16
+            self.frameMultiplier = self.TR * continous_multiplier
             self.nContinousFrames = int(self.nFrames * self.frameMultiplier)
             self.continousBlankLength = int(blank_duration * self.frameMultiplier)
             self.framesPerCrossing = int(
@@ -167,6 +172,8 @@ class barStimulus(Stimulus):
                 self.nContinousFrames
             )  # to find which checkerboard to use
 
+            self.jump_size = self.overlap * self.bar_width / self.frameMultiplier
+
             it = 0
             for cross in self.startingDirection:
                 for i in range(self.framesPerCrossing):
@@ -175,20 +182,12 @@ class barStimulus(Stimulus):
                         :,
                         max(
                             0,
-                            int(
-                                self.overlap *
-                                self.barWidth /
-                                self.frameMultiplier *
-                                (i - self.frameMultiplier * 2)
-                            ),
+                            int(self.jump_size * (i - self.frameMultiplier)),
                         ) : min(
                             self._stimSize,
                             int(
-                                self.overlap *
-                                self.barWidth /
-                                self.frameMultiplier *
-                                (i - self.frameMultiplier * 2) +
-                                self.barWidth
+                                self.jump_size * (i - self.frameMultiplier)
+                                + self.bar_width
                             ),
                         ),
                     ] = 1
@@ -198,8 +197,8 @@ class barStimulus(Stimulus):
                     #     frame2 = np.zeros((self._stimSize,self._stimSize))
 
                     #     for nbar in range(self.nBars-1) :
-                    #         o = int(self.overlap*self.barWidth*(i-1) + self.nBarShift*(nbar+1))
-                    #         t = int(self.overlap*self.barWidth*(i-1) + self.barWidth*self.thickRatio + self.nBarShift*(nbar+1))
+                    #         o = int(self.overlap*self.bar_width*(i-1) + self.nBarShift*(nbar+1))
+                    #         t = int(self.overlap*self.bar_width*(i-1) + self.bar_width*self.thickRatio + self.nBarShift*(nbar+1))
                     #         if o>self._stimSize: o -= self._stimSize
                     #         if t>self._stimSize: t -= self._stimSize
                     #         frame2[:, max(0, o):min(self._stimSize, t)] = 1
@@ -222,26 +221,33 @@ class barStimulus(Stimulus):
             self._stimUnc = np.zeros(self._stimRaw.shape)
             self._stimUnc[:, self._stimMask] = self._stimRaw[:, self._stimMask]
 
-
     def _checkerboard(self, nChecks=10):
         """create the four flickering main images"""
 
         # we want at least 1.5 checkers per bar width
-        self.checkSize = np.min((np.ceil(self._stimSize / nChecks / 2).astype(int),
-                                 np.ceil(self.barWidth/1.5))).astype(int)
-        self.nChecks = np.ceil(self._stimSize / self.checkSize / 2).astype(int)
+        if hasattr(self, "bar_width"):
+            self.checkSize = np.min(
+                (
+                    np.ceil(self._stimSize / nChecks / 2).astype(int),
+                    np.ceil(self.bar_width / 1.5),
+                )
+            ).astype(int)
+            self.nChecks = np.ceil(self._stimSize / self.checkSize / 2).astype(int)
+        else:
+            self.checkSize = np.ceil(self._stimSize / nChecks / 2).astype(int)
+            self.nChecks = nChecks
 
         self.checkA = np.kron(
-            [[0, 255] * (self.nChecks + 1), [255, 0] * (self.nChecks + 1)] *
-            (self.nChecks + 1),
+            [[0, 255] * (self.nChecks + 1), [255, 0] * (self.nChecks + 1)]
+            * (self.nChecks + 1),
             np.ones((self.checkSize, self.checkSize)),
         )[
             int(self.checkSize * 3 / 2) : -int(self.checkSize / 2),
             int(self.checkSize / 2) : -int(self.checkSize * 3 / 2),
         ]
         self.checkB = np.kron(
-            [[255, 0] * (self.nChecks + 1), [0, 255] * (self.nChecks + 1)] *
-            (self.nChecks + 1),
+            [[255, 0] * (self.nChecks + 1), [0, 255] * (self.nChecks + 1)]
+            * (self.nChecks + 1),
             np.ones((self.checkSize, self.checkSize)),
         )[
             int(self.checkSize * 3 / 2) : -int(self.checkSize / 2),
@@ -251,33 +257,33 @@ class barStimulus(Stimulus):
         self.checkC = np.where(
             skiT.rotate(
                 np.kron(
-                    [[0, 255] * (self.nChecks + 1), [255, 0] * (self.nChecks + 1)] *
-                    (self.nChecks + 1),
+                    [[0, 255] * (self.nChecks + 1), [255, 0] * (self.nChecks + 1)]
+                    * (self.nChecks + 1),
                     np.ones((self.checkSize, self.checkSize)),
                 ),
                 angle=45,
                 resize=False,
                 order=0,
-            ) <
-            128,
+            )
+            < 128,
             0,
             255,
-        )[self.checkSize:-self.checkSize, self.checkSize:-self.checkSize]
+        )[self.checkSize : -self.checkSize, self.checkSize : -self.checkSize]
         self.checkD = np.where(
             skiT.rotate(
                 np.kron(
-                    [[255, 0] * (self.nChecks + 1), [0, 255] * (self.nChecks + 1)] *
-                    (self.nChecks + 1),
+                    [[255, 0] * (self.nChecks + 1), [0, 255] * (self.nChecks + 1)]
+                    * (self.nChecks + 1),
                     np.ones((self.checkSize, self.checkSize)),
                 ),
                 angle=45,
                 resize=False,
                 order=0,
-            ) <
-            128,
+            )
+            < 128,
             0,
             255,
-        )[self.checkSize:-self.checkSize, self.checkSize:-self.checkSize]
+        )[self.checkSize : -self.checkSize, self.checkSize : -self.checkSize]
 
         if self.checkA.shape[0] != self._stimSize:
             diff = (self.checkA.shape[0] - self._stimSize) / 2
@@ -302,12 +308,14 @@ class barStimulus(Stimulus):
 
     def stimulus_length(self):
         super().stimulus_length()
-        print(f'Frames per crossing: {self.framesPerCrossing} TRs')
-        print(f'blanks: {self.nBlanks} x {self.blankLength} TRs')
+        print(f"Frames per crossing: {self.framesPerCrossing} TRs")
+        print(f"blanks: {self.nBlanks} x {self.blankLength} TRs")
 
     def bar_params(self):
         if self.nBars > 1:
-            print(f'nBars: {self.nBars}')
-            print(f'shift between bars: {self.nBarShift}')
-            print(f'bar rotation: {self.doubleBarRot}')
-        print(f'Bar width: {np.float64(np.round(self.barWidth / self._stimSize * self._maxEcc * 2, 2))}°')
+            print(f"nBars: {self.nBars}")
+            print(f"shift between bars: {self.nBarShift}")
+            print(f"bar rotation: {self.doubleBarRot}")
+        print(
+            f"Bar width: {np.float64(np.round(self.bar_width / self._stimSize * self._maxEcc * 2, 2))}°"
+        )
